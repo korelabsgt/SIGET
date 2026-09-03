e4d79be67a057eea61369e9686a7bd77a93440a9
Angel10Mata <angelmata112@gmail.com>
2026-09-03 09:38:37 -0600

    modificaciones

--- src/components/(SIGET)/gestion-territorial/gestion-vehiculos/solicitudes/SolicitudesNotificaciones.tsx
+++ src/components/(SIGET)/gestion-territorial/gestion-vehiculos/solicitudes/SolicitudesNotificaciones.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { AlertTriangle, Bell, BellRing, CalendarClock, Clock } from "lucide";
+import { Bell, BellRing, CalendarClock, Clock } from "lucide";
+import { AlertTriangle } from "lucide-react";
 import { GvMorphIcon } from "../lib/morph-icon";
 import {
   DropdownMenu,
